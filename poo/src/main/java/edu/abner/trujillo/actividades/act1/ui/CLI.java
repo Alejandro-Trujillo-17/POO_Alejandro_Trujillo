ca7b4589a04bad9bf3b68e59540927b7008b1bb4
Alejandro-Trujillo-17 <ccastxdxd@gmail.com>
2025-01-14 11:58:45 -0600

    commit

--- poo/src/main/java/edu/abner/trujillo/actividades/act1/ui/CLI.java
+++ poo/src/main/java/edu/abner/trujillo/actividades/act1/ui/CLI.java
@@ -3,7 +3,7 @@ package edu.abner.trujillo.actividades.act1.ui;
 public class CLI {
     public static void ShowMenu() {
         System.out.println("Menu");
-        System.out.println("Menu");
+        System.out.println("ahgsjdfbkdlfh");
         System.out.println("Menu");
     }
 }
