514a1907aeffda961bd5c8349717a39c24c3763e
Alejandro-Trujillo-17 <ccastxdxd@gmail.com>
2025-01-14 07:22:08 -0600

    commit

---
+++ src/carlos/castillo/actividades/actividad1/init/Main.java
@@ -0,0 +1,9 @@
+package carlos.castillo.actividades.actividad1.init;
+
+import carlos.castillo.actividades.actividad1.ui.CLI;
+
+public class Main {
+    public static void main(String[] args) {
+        CLI.startCLI();
+    }
+}
---
+++ src/carlos/castillo/actividades/actividad1/ui/CLI.java
@@ -0,0 +1,7 @@
+package carlos.castillo.actividades.actividad1.ui;
+
+public class CLI {
+    public static void startCLI() {
+        System.out.println("this class is not implemented yet");
+    }
+}
