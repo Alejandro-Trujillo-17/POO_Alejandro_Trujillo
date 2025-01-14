6c5faf7a6c5f4cfc2e04e66d149cac0192c8e931
Alejandro-Trujillo-17 <ccastxdxd@gmail.com>
2025-01-14 11:52:41 -0600

    commit

---
+++ poo/src/main/java/edu/abner/trujillo/actividades/act1/Starter.java
@@ -0,0 +1,9 @@
+package edu.abner.trujillo.actividades.act1;
+
+import edu.abner.trujillo.actividades.act1.ui.CLI;
+
+public class Starter {
+public static void main(String[] args) {
+    CLI.ShowMenu();
+}
+}
---
+++ poo/src/main/java/edu/abner/trujillo/actividades/act1/ui/CLI.java
@@ -0,0 +1,9 @@
+package edu.abner.trujillo.actividades.act1.ui;
+
+public class CLI {
+    public static void ShowMenu() {
+        System.out.println("Menu");
+        System.out.println("Menu");
+        System.out.println("Menu");
+    }
+}
