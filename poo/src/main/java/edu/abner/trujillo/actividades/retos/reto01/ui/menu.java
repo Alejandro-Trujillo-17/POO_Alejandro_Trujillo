40fee1a42a27027b80457df519ac5a70ecb99c33
Alejandro-Trujillo-17 <ccastxdxd@gmail.com>
2025-01-21 11:46:53 -0600

    commit

---
+++ poo/src/main/java/edu/abner/trujillo/actividades/retos/reto01/ui/menu.java
@@ -0,0 +1,9 @@
+package edu.abner.trujillo.actividades.retos.reto01.ui;
+import edu.abner.trujillo.actividades.retos.reto01.process.Reverse;
+
+public class menu {
+    public static void runApp() {
+        Reverse reverse = new Reverse();
+        System.out.println(reverse);
+    }
+}
