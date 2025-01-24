e9592f331e50a8eb45c3131061c508111d556e8c
Alejandro-Trujillo-17 <ccastxdxd@gmail.com>
2025-01-23 19:07:20 -0600

    testeo commit

--- poo/src/main/java/edu/abner/trujillo/actividades/act1/Starteer.java
+++ poo/src/main/java/edu/abner/trujillo/actividades/act1/Starteer.java
@@ -2,7 +2,7 @@ package edu.abner.trujillo.actividades.act1;
 
 import edu.abner.trujillo.actividades.act1.ui.CLI;
 
-public class Starter {
+public class Starteer {
 public static void main(String[] args) {
     
     CLI.runApp();
