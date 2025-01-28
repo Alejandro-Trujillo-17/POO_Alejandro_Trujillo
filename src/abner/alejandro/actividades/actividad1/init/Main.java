c66188bd38c9f3bd0e6d6a74e11a48af2d8d9914
Alejandro-Trujillo-17 <ccastxdxd@gmail.com>
2025-01-28 08:00:05 -0600

    limpiando Repositorio commit

--- src/abner/alejandro/actividades/actividad1/init/Main.java
+++
@@ -1,9 +0,0 @@
-package abner.alejandro.actividades.actividad1.init;
-
-import abner.alejandro.actividades.actividad1.ui.CLI;
-
-public class Main {
-    public static void main(String[] args) {
-        CLI.startCLI();
-    }
-}
--- src/abner/alejandro/actividades/actividad1/ui/CLI.java
+++
@@ -1,7 +0,0 @@
-package abner.alejandro.actividades.actividad1.ui;
-
-public class CLI {
-    public static void startCLI() {
-        System.out.println("this class is not implemented yet");
-    }
-}
