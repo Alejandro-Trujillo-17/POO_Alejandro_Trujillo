9702eb7b15686f75ae1863ce4c6453c89fa185cd
Alejandro-Trujillo-17 <ccastxdxd@gmail.com>
2025-01-28 10:46:08 -0600

    corrected typo in Starter.java

--- poo/src/main/java/edu/abner/trujillo/actividades/act3/Starter.java
+++ poo/src/main/java/edu/abner/trujillo/actividades/act3/Starter.java
@@ -2,7 +2,7 @@ package edu.abner.trujillo.actividades.act3;
 
 import edu.abner.trujillo.actividades.act3.ui.CLII;
 
-public class Starteer {
+public class Starter {
 public static void main(String[] args) {
     
     CLII.runApp();
